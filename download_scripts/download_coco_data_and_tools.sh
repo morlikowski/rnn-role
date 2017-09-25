a444f58fd38675430faac155383408b46c570d41
Matthias Orlikowski <mazsvr@gmail.com>
2017-09-25 17:52:45 +0200

    Add COCO generated captions for evaluation to download script

--- download_scripts/download_coco_data_and_tools.sh
+++ download_scripts/download_coco_data_and_tools.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
-# TODO check if data set from http://cs.stanford.edu/people/karpathy/deepimagesent/
-# can be used with not too much adaptions to code
-
 git clone https://github.com/tylin/coco-caption.git
 mv coco-caption/pycocotools pycocotools
 mv coco-caption/pycocoevalcap pycocoevalcap
-mkdir -p data/coco/
-mv coco-caption/annotations/ data/coco/
+mkdir -p data/coco/annotations/
+mkdir data/coco/results/
+mv coco-caption/annotations/captions_val2014.json data/coco/annotations/captions.json
+mv coco-caption/results/captions_val2014_fakecap_results.json data/coco/results/generated_captions.json
 rm -fR coco-caption
